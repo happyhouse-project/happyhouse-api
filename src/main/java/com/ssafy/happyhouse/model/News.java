ab90588284b7f820a0cb7a4a52b080b357c9c11e
hansol kim <doingnow94@gmail.com>
2020-11-09 01:02:13 +0900

    News 모델 및 Mapper XML파일 생성

---
+++ src/main/java/com/ssafy/happyhouse/model/News.java
@@ -0,0 +1,39 @@
+package com.ssafy.happyhouse.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/** 아파트 및 주택정보 관련 뉴스 모델 */
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class News {
+
+	/** 뉴스 관리 id */
+	private int id;
+
+	/** 뉴스 제목 */
+	private String title;
+
+	/** 뉴스 링크 */
+	private String link;
+
+	/** 뉴스 등록자 */
+	private String register;
+
+	/** 뉴스 등록자 id */
+	private int registerId;
+
+	/** 뉴스 등록일자 */
+	private String registerDate;
+
+	/** 카테고리 */
+	private String category;
+
+	/** 크롤링 뉴스 테이블 id */
+	private int newsId;
+}
