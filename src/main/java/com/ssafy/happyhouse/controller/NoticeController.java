9998153fca2d8b72f325981af5f3f995e7645312
hansol kim <doingnow94@gmail.com>
2020-11-13 01:01:28 +0900

    [REST변경] NoticeController - noticeList junit test코드 작성(TDD)

--- src/main/java/com/ssafy/happyhouse/controller/NoticeController.java
+++ src/main/java/com/ssafy/happyhouse/controller/NoticeController.java
@@ -15,76 +15,60 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.happyhouse.model.Notice;
 import com.ssafy.happyhouse.service.NoticeService;
 
-@Controller
+@RestController
 @RequestMapping("/notice")
 public class NoticeController {
 
-	@Autowired
-	private NoticeService noticeService;
-	
-	private final static String PATH = "notice/";
+	/*
+	 * @Autowired private NoticeService noticeService;
+	 * 
+	 * private final static String PATH = "notice/";
+	 */
 	
 	@GetMapping("/list")
-	public String list(Model model) {
-		List<Notice> list = noticeService.selectAll();
-		model.addAttribute("list", list);
-		return PATH+"noticeList";
+	public List<Notice> list() {
+		List<Notice> notices = new ArrayList<>();
+		Notice notice = new Notice(0, "test공지", "test", "admin", "2020-11-13");
+		notices.add(notice);
+		return notices;
 	}
 	
-	@GetMapping("/mainNoticeList")
-	@ResponseBody
-	public String mainList(Model model) {
-		ArrayList<Notice> list = new ArrayList<>();
-		JSONArray arr = new JSONArray();
-		try {
-			list = noticeService.selectByLimit7();
-			for(Notice notice : list) {
-				JSONObject obj = new JSONObject();
-				obj.put("id", notice.getId());
-				obj.put("title", notice.getTitle());
-				arr.add(obj);
-			}
-		} catch (Exception e) {
-			arr = new JSONArray();
-			JSONObject obj = new JSONObject();
-			obj.put("message_code", "-1");
-			arr.add(obj);
-			e.printStackTrace();
-		} finally {
-			return arr.toJSONString();
-		}
-	}
-	
-	@GetMapping("/register")
-	public String register(Model model) {
-		return PATH+"noticeRegister";
-	}
-
-	@PostMapping("registerProcess")
-	public String registerProcess(@ModelAttribute Notice notice) {
-		noticeService.insert(notice);
-		return "redirect:/"+PATH+"list";
-	}
-	
-	@GetMapping(value = "/detail")
-	public String read(Model model, HttpServletRequest req) {
-		int searchId = Integer.parseInt(req.getParameter("id"));
-		Notice notice = noticeService.selectOne(searchId);
-		
-		model.addAttribute("notice", notice);
-		return PATH+"noticeDetail";
-	}
-	
-	@GetMapping(value = "/delete")
-	public String delete(HttpServletRequest req) {
-		int deleteId = Integer.parseInt(req.getParameter("id"));
-		noticeService.delete(deleteId);
-		
-		return "redirect:/"+PATH+"list";
-	}
+	/*
+	 * @GetMapping("/mainNoticeList")
+	 * 
+	 * @ResponseBody public String mainList(Model model) { ArrayList<Notice> list =
+	 * new ArrayList<>(); JSONArray arr = new JSONArray(); try { list =
+	 * noticeService.selectByLimit7(); for(Notice notice : list) { JSONObject obj =
+	 * new JSONObject(); obj.put("id", notice.getId()); obj.put("title",
+	 * notice.getTitle()); arr.add(obj); } } catch (Exception e) { arr = new
+	 * JSONArray(); JSONObject obj = new JSONObject(); obj.put("message_code",
+	 * "-1"); arr.add(obj); e.printStackTrace(); } finally { return
+	 * arr.toJSONString(); } }
+	 * 
+	 * @GetMapping("/register") public String register(Model model) { return
+	 * PATH+"noticeRegister"; }
+	 * 
+	 * @PostMapping("registerProcess") public String registerProcess(@ModelAttribute
+	 * Notice notice) { noticeService.insert(notice); return
+	 * "redirect:/"+PATH+"list"; }
+	 * 
+	 * @GetMapping(value = "/detail") public String read(Model model,
+	 * HttpServletRequest req) { int searchId =
+	 * Integer.parseInt(req.getParameter("id")); Notice notice =
+	 * noticeService.selectOne(searchId);
+	 * 
+	 * model.addAttribute("notice", notice); return PATH+"noticeDetail"; }
+	 * 
+	 * @GetMapping(value = "/delete") public String delete(HttpServletRequest req) {
+	 * int deleteId = Integer.parseInt(req.getParameter("id"));
+	 * noticeService.delete(deleteId);
+	 * 
+	 * return "redirect:/"+PATH+"list"; }
+	 */
 	
 }
---
+++ src/test/java/com/ssafy/happyhouse/controller/NoticeControllerTest.java
@@ -0,0 +1,48 @@
+package com.ssafy.happyhouse.controller;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(NoticeController.class)
+class NoticeControllerTest {
+
+	@Autowired
+	private MockMvc mvc;
+	
+	@Autowired
+    private WebApplicationContext ctx;
+	
+	@BeforeEach
+    public void setup() {
+        this.mvc = MockMvcBuilders.webAppContextSetup(ctx)
+                .addFilters(new CharacterEncodingFilter("UTF-8", true))  // 필터 추가
+                .alwaysDo(print())
+                .build();
+    }
+	
+	@Test
+	public void list() throws Exception {
+		mvc.perform(get("/notice/list"))
+			.andExpect(status().isOk())
+			.andExpect(content().string(
+					containsString("\"title\":\"test공지\"")))
+			.andExpect(content().string(
+					containsString("\"content\":\"test\"")));
+	}
+
+}
