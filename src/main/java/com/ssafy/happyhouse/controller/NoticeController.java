ebc158a236d36de0ccf9c136f31efa604ee89373
hansol kim <doingnow94@gmail.com>
2020-11-19 15:21:23 +0900

    @CrossOrigin 추가

--- src/main/java/com/ssafy/happyhouse/controller/NoticeController.java
+++ src/main/java/com/ssafy/happyhouse/controller/NoticeController.java
@@ -2,32 +2,25 @@ package com.ssafy.happyhouse.controller;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.happyhouse.model.Notice;
 import com.ssafy.happyhouse.service.NoticeService;
 
 @RestController
+@CrossOrigin("*")
 public class NoticeController {
 
 	@Autowired
